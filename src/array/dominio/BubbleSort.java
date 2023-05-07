984acffe2a71e0388c58145184c0e8e8b96c6b40
durexex <eduardodurieuxlopes@gmail.com>
2023-05-07 19:10:35 -0300

    Array correção

--- src/array/dominio/BubbleSort.java
+++ src/array/dominio/BubbleSort.java
@@ -14,7 +14,7 @@ public class BubbleSort {
 		for (int i = 0; i < tamanho - 1; i++) {
 			for (int j = 0; j < tamanho - i - 1; j++) {
 				if (array[j] > array[j + 1]) {
-					int temp = array[j];
+					int temp = array[j + 1];
 					array[j + 1] = array[j];
 					array[j] = temp;
 				}
